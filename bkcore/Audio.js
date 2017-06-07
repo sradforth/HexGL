e72ad97a0fa5a1604b0af3accc55b59cc1049b45
Stuart Radforth <stuart.radforth@aszart.com>
2017-06-07 23:01:30 +0100

    Remove audio error

--- bkcore/Audio.js
+++ bkcore/Audio.js
@@ -137,6 +137,9 @@ bkcore.Audio.setListenerPos = function(vec){
 bkcore.Audio.setListenerVelocity = function(vec){
 	if(bkcore.Audio._ctx){
 		var panner = bkcore.Audio._panner;
-		panner.setVelocity(vec.x, vec.y, vec.z);
+		if( panner.setVelocity != null )
+		{
+			panner.setVelocity(vec.x, vec.y, vec.z);
+		}		
 	}
 };
